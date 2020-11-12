507af41756cf1e173466546a14401b13cc94d036
andlap24 <andrew.laptev24@gmail.com>
2020-11-12 15:12:42 +0200

    Updates

---
+++ precache-manifest.c5b0749a1fdb42bba7778d3780c37fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903bf03b9d6f20e9d3de7acf955e83fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "727cc480c9346859c5df",
+    "url": "./static/css/main.1b11e97d.chunk.css"
+  },
+  {
+    "revision": "35d9e164ddcbdf3f4d9d",
+    "url": "./static/js/2.133e1979.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.133e1979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727cc480c9346859c5df",
+    "url": "./static/js/main.4b8b697b.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  },
+  {
+    "revision": "6092516ea927337ac66e6f3000a87d4e",
+    "url": "./static/media/close-btn.6092516e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.c5b0749a1fdb42bba7778d3780c37fb2.js"
 );
 
 self.addEventListener('message', (event) => {
